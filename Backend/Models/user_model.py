47facfa23efee2924e0e481ee2a8a074443bd022
Iyosiyas <iyosieyosiyas@gmail.com>
2026-02-25 21:17:44 +0300

    setup the user model

---
+++ Backend/Models/user_model.py
@@ -0,0 +1,11 @@
+from sqlalchemy import Column, String, Integer, ForeignKey, DateTime
+from sqlalchemy.orm import relationship
+from app.database import Base
+from datetime import datetime
+
+class User(Base):
+    __tablename__ = "users"
+    id = Column(Integer, primary_key=True, index=True, nullable=False)
+    email = Column(String, nullable=False)
+    password = Column(String, nullable=False)
+    created_at = Column(DateTime, default=datetime.utcnow)
