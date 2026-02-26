3af2d72ab753126b118220eaf35cb15a0ad64768
Iyosiyas <iyosieyosiyas@gmail.com>
2026-02-26 12:47:15 +0300

    setup the database connetion

---
+++ Backend/main.py
@@ -0,0 +1,11 @@
+# main.py
+import uvicorn
+from fastapi import FastAPI
+from database import Base, engine
+
+app = FastAPI(title="Closet Mate")
+
+Base.metadata.create_all(bind=engine)
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
