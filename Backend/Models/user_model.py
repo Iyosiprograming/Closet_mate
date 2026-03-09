80f9a7362c12705d5b34c2aeb59d5558b91dd90e
Iyosiyas <iyosieyosiyas@gmail.com>
2026-03-09 10:30:51 +0300

    setup the user and clothe model

---
+++ Backend/Models/user_model.py
@@ -0,0 +1,13 @@
+from sqlalchemy import Column, String, UUID, DateTime
+from sqlalchemy.dialects.postgresql import UUID
+from sqlalchemy.orm import relationship
+from database import Base
+from datetime import datetime
+
+class User(Base):
+    __tablename__ = "users"
+    id= Column(UUID(as_uuid=True), primary_key=True, index=True, default=uuid.uuid4)
+    email = Column(String, unique=True, index=True)
+    password = Column(String)
+    created_at = Column(DateTime, default=datetime.utcnow)
+    clothes = relationship("Clothes", back_populates="user")
