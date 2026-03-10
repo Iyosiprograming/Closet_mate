bfe9758c57d914e2fb88b31a6982785d8f26af8f
Iyosiyas <iyosieyosiyas@gmail.com>
2026-03-10 08:23:30 +0300

    setup user login and small fixes

--- Backend/Controllers/user_controller.py
+++ Backend/Controllers/user_controller.py
@@ -1,8 +1,9 @@
-from fastapi import HTTPException, status
+from fastapi import HTTPException, status, Cookie, Response
 from sqlalchemy.orm import Session
 from Models.user_model import User
-from Services.password import hashPassword
-from Schema.user_schema import CreateUser, CreateUserResponse
+from Services.password import hashPassword, verifyhashPassword
+from Services.jwt import create_access_token, verify_token
+from Schema.user_schema import CreateUser, CreateUserResponse,LoginUser,LoginUserResponse
 
 def register_user(user: CreateUser, db: Session) -> CreateUserResponse:
     existing_user = db.query(User).filter(User.email == user.email).first()
@@ -12,7 +13,7 @@ def register_user(user: CreateUser, db: Session) -> CreateUserResponse:
             detail="User already exists"
         )
     
-    hashed_password = hash(user.password)
+    hashed_password = hashPassword(user.password)
     new_user = User(
         email=user.email,
         password=hashed_password
@@ -27,5 +28,30 @@ def register_user(user: CreateUser, db: Session) -> CreateUserResponse:
             "email": new_user.email,
             "status_code": status.HTTP_201_CREATED,
             "message": "User created successfully"
+        }
+    )
+
+def login_user(user:LoginUser, response:Response, db:Session):
+    existing_user = db.query(User).filter(User.email == user.email).first()
+    if not existing_user:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Invalid credentials")
+    if not verifyhashPassword(user.password, existing_user.password):
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Invalid credentials")
+
+    access_token = create_access_token({"user_id":str(existing_user.id)})
+
+    response.set_cookie(
+        key="access_token",
+        value=access_token,
+        httponly=True,
+        max_age=1800,
+        samesite="lax"
+    )
+    return LoginUserResponse(
+        detail={
+            "user_id": str(existing_user.id),
+            "email": existing_user.email,
+            "status_code": status.HTTP_200_OK,
+            "message": "User logged in successfully"
         }
     )
--- Backend/Routers/user_router.py
+++ Backend/Routers/user_router.py
@@ -1,11 +1,15 @@
-from fastapi import APIRouter, Depends
-from Controllers.user_controller import register_user
-from Schema.user_schema import CreateUser, CreateUserResponse
+from fastapi import APIRouter, Depends,Response, status
+from Controllers.user_controller import register_user, login_user
+from Schema.user_schema import CreateUser, CreateUserResponse,LoginUser,LoginUserResponse
 from database import get_db
 from sqlalchemy.orm import Session
 
 router = APIRouter(prefix="/users", tags=["users"])
 
-@router.post("/", response_model=CreateUserResponse)
-def create_user(user: CreateUser, db: Session = Depends(get_db)):
+@router.post("/", response_model=CreateUserResponse , status_code=status.HTTP_201_CREATED)
+def create_user_endpoint(user: CreateUser, db: Session = Depends(get_db)):
     return register_user(user, db)
+
+@router.post("/login", response_model=LoginUserResponse, status_code=status.HTTP_200_OK)
+def login_user_endpoint(user:LoginUser, response:Response, db: Session = Depends(get_db)):
+        return login_user(user, response, db)
